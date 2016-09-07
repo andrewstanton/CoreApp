a541b6e76fbb2b39f367297124d9808ebae7803f
Andrew Stanton <andrew@toddprod.com>
2016-09-06 23:00:23 -0400

    Added Path to Config File and Removed Application specific file

--- CoreLog.php
+++ CoreLog.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once('Config.php');
+require_once('../Config.php');
 require_once('CoreDebug.php');
 
 class CoreLog {
 
     /**
      * Toggle logging of normal events.
-     * 
+     *
      * @var boolean
      */
     private static $enabled = true;
@@ -16,14 +16,14 @@ class CoreLog {
      * Toggle debugging- these are events that shouldn't happen.
      * If they do they indicate a need to debug the problem.
      * They are thrown in production.
-     * 
+     *
      * @var boolean
      */
     private static $debugEnabled = true;
-    
+
     /**
      * General log
-     * 
+     *
      * @param string $msg
      * @param string $logName
      * @return
@@ -38,7 +38,7 @@ class CoreLog {
 
     /**
      * Events that should never happen and may indicate an error.
-     * 
+     *
      * @param string $msg
      * @param string $logName
      * @return
@@ -50,10 +50,10 @@ class CoreLog {
 
         self::createLog($msg, $logName);
     }
-    
+
     /**
-     * Automated tasks output log. 
-     * 
+     * Automated tasks output log.
+     *
      * @param string $msg
      * @param string $logName
      * @return
@@ -61,21 +61,21 @@ class CoreLog {
     public static function cron($msg, $logName = 'cron_log') {
         self::createLog($msg, $logName);
     }
-    
+
     /**
      * Creates a trace of an error in the trace log.
-     * Used for testing and debugging. 
-     * 
+     * Used for testing and debugging.
+     *
      * @return
      */
     public static function trace(){
-        self::createLog(CoreDebug::backtrace(debug_backtrace()) , 'trace_log');           
+        self::createLog(CoreDebug::backtrace(debug_backtrace()) , 'trace_log');
     }
-    
-    
+
+
     /**
      * Authorization protocol logging
-     * 
+     *
      * @param string $msg
      * @param string $logName
      * @return
@@ -83,16 +83,16 @@ class CoreLog {
     public static function auth($msg, $logName = 'authorization_log') {
         self::createLog($msg, $logName);
     }
-    
+
     /**
-     * Creates a log entry in the specified log. 
-     * 
+     * Creates a log entry in the specified log.
+     *
      * @param string $msg
      * @param string $logName
      * @return
      */
     private static function createLog($msg, $logName = 'debug_log') {
-		 
+
         $file = Config::$LOG_PATH . $logName . ' ' . date('Y-m-d') . '.log';
 
         $logMsg = date('Y-m-d H:i:s') . ";  ";
